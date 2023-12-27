83e47f0ca80efd9de919831eb6db5de7cdcf9426
Zoont <89505339+ZoontS@users.noreply.github.com>
2023-12-28 00:40:25 +0700

    Update Models.kt

--- app/src/main/java/com/example/supershop/data/room/models/Models.kt
+++ app/src/main/java/com/example/supershop/data/room/models/Models.kt
@@ -1,7 +1,39 @@
 package com.example.supershop.data.room.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "shopping_list")
 data class ShoppingList(
+    @ColumnInfo(name = "list_id")
+    @PrimaryKey
     val id: Int,
-    val name: String,
-//    val items: List<ShoppingListItem>
+    val name: String
+)
+
+@Entity(tableName = "item")
+data class Item(
+    @ColumnInfo(name = "item_id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val itemName: String,
+    val quantity: Int,
+    val listIdFk: Int,
+    val storeIdFk: Int,
+    val date: Date,
+    val isChecked: Boolean,
+)
+
+@Entity(tableName = "store")
+data class Store(
+    @ColumnInfo(name = "store_id")
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val listIdFk: Int,
+    val storeName: String,
+    val address: String,
+    val city: String,
+    val state: String,
 )
