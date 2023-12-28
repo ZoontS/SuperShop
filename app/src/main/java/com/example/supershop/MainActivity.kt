33c7ad7e8ce67795d841c08fe99e4ddca55cc71e
Zoont <89505339+ZoontS@users.noreply.github.com>
2023-12-29 02:37:36 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/supershop/MainActivity.kt
+++ app/src/main/java/com/example/supershop/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.supershop.ui.home.HomeScreen
 import com.example.supershop.ui.theme.SuperShopTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen(onNavigate = {})
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DefaultPreview() {
     SuperShopTheme {
-        Greeting("Android")
+
     }
 }
