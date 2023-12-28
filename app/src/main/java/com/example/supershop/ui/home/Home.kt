d28e4de496de9fd4e480e750afbd3156d280d12b
Zoont <89505339+ZoontS@users.noreply.github.com>
2023-12-29 02:37:38 +0700

    Update Home.kt

--- app/src/main/java/com/example/supershop/ui/home/Home.kt
+++ app/src/main/java/com/example/supershop/ui/home/Home.kt
@@ -1,13 +1,15 @@
 package com.example.supershop.ui.home
 
 import android.annotation.SuppressLint
-import android.icu.text.CaseMap.Title
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -19,22 +21,29 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.supershop.data.room.ItemsWithStoreAndList
+import com.example.supershop.data.room.models.Item
 import com.example.supershop.ui.Category
 import com.example.supershop.ui.Utils
 import com.example.supershop.ui.theme.Shapes
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
@@ -63,6 +72,15 @@ fun HomeScreen(
                         }
                         Spacer(modifier = Modifier.size(16.dp))
                     }
+                }
+            }
+            items(homeState.items) {
+                ShoppingItems(
+                    item = it,
+                    isChecked = it.item.isChecked,
+                    onCheckedChange = homeViewModel::onItemCheckedChange
+                ) {
+                    onNavigate.invoke(it.item.id)
                 }
             }
         }
@@ -96,7 +114,8 @@ fun CategoryItem(
             contentColor = MaterialTheme.colorScheme.onPrimary
         )
         else CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.onSurface
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
         )
     ) {
         Row(
@@ -118,3 +137,59 @@ fun CategoryItem(
         }
     }
 }
+
+@Composable
+fun ShoppingItems(
+    item: ItemsWithStoreAndList,
+    isChecked: Boolean,
+    onCheckedChange: (Item, Boolean) -> Unit,
+    onItemClick: () -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick.invoke() }
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = item.item.itemName,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Text(text = item.store.storeName)
+                Spacer(modifier = Modifier.size(4.dp))
+                CompositionLocalProvider(
+                    LocalContentColor provides MaterialTheme.colorScheme.onSurface.copy(
+                        alpha = 0.38f
+                    )
+                ) {
+                    Text(
+                        text = formatDate(item.item.date),
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+            }
+            Column(modifier = Modifier.padding(8.dp)) {
+                Text(
+                    text = "Qty: ${item.item.quantity}",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.size(4.dp))
+                Checkbox(
+                    checked = isChecked,
+                    onCheckedChange = { onCheckedChange.invoke(item.item, it) })
+            }
+        }
+    }
+}
+
+fun formatDate(date: Date): String =
+    SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(date)
