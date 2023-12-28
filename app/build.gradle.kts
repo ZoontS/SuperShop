c0dfa8ac4271878744af54d66725c63ba352d860
Zoont <89505339+ZoontS@users.noreply.github.com>
2023-12-29 01:40:26 +0700

    Update Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
